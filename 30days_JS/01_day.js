97a86e6547e732303fde99787eb21fb8e4a39958
advait-git <advaittiwari2@gmail.com>
2024-07-18 12:49:26 +0530

    day 1 code challenge

---
+++ 30days_JS/01_day.js
@@ -0,0 +1,41 @@
+//activity 1
+var code = "JS"
+console.log(code);
+
+let chai = '5'
+console.log(chai);
+
+//activity 2
+
+const boolean = true;
+console.log(boolean);
+
+//activity 3
+
+const data1 = "advait";
+console.log(typeof data1);
+const data2 = 21
+console.log(typeof data2);
+const data3 = {name : "advait"}
+console.log(typeof data3);
+let data4 = [1,2,3,4,5];
+console.log(typeof data4);
+
+//activity 4
+
+let language = "java";
+
+console.log(language);
+
+ language = "JS";
+console.log(language);
+
+//activity 5
+
+const add = "something";
+console.log(add);
+
+add = "nothing";
+
+//add = "nothing";
+// TypeError: Assignment to constant variable.
